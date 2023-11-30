8d8ce02d54809e75d74d2114a2665eed29437412
Jimil1407 <92706888+Jimil1407@users.noreply.github.com>
2023-11-30 10:10:45 +0530

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,44 +14,39 @@ const Home = () => {
 
   const imageSpring = useSpring({
     opacity: 1,
-    transform: "translateX(10%) rotate(0deg)", // Adjust the translateX value
+    transform: "translateX(10%) rotate(0deg)",
     from: { opacity: 0, transform: "translateX(50%) rotate(45deg)" },
     delay: 1000,
   });
 
   return (
-    <div
-      name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 flex items-center justify-center"
-    >
-      <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row p-8"> {/* Added vertical padding */}
-        <div className="flex flex-col justify-center h-full w-full md:w-1/2">
+    <div id = "home" className="bg-gradient-to-b from-black via-black to-gray-800 min-h-screen flex items-center">
+      <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row p-8 items-center">
+        <div className="flex flex-col justify-center w-full md:w-1/2 mt-12 md:mt-0">
           <animated.h2
-            className="text-4xl sm:text-7xl font-bold text-white"
+            className="text-4xl sm:text-7xl font-bold text-white text-center mb-4"
             style={textSpring}
           >
             Machine Learning & Full Stack Developer
           </animated.h2>
           <animated.p
-            className="text-gray-500 py-4 max-w-md"
+            className="text-gray-500 max-w-md text-center mb-4"
             style={textSpring}
           >
             I'm a Computer Science undergrad passionate about AI and Machine Learning. With hands-on experience in building Machine Learning Models, I also enjoy creating user-friendly web applications using HTML, JavaScript, React, and Tailwind. Let's connect and explore opportunities!
           </animated.p>
 
-          <div>
-            <Link
-              to="portfolio"
-              smooth
-              duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
-            >
-              Portfolio
-              <span className="group-hover:rotate-90 duration-300">
-                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
-              </span>
-            </Link>
-          </div>
+          <Link
+            to="portfolio"
+            smooth
+            duration={500}
+            className="group text-white w-fit px-6 py-3 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer mx-auto md:mx-0"
+          >
+            Portfolio
+            <span className="group-hover:rotate-90 duration-300">
+              <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+            </span>
+          </Link>
         </div>
 
         <animated.div style={imageSpring} className="w-full md:w-1/2">
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,32 +3,60 @@ import price from '../assets/portfolio/bitcoin.jpeg';
 import spam from '../assets/portfolio/spam.jpg';
 import ticket from '../assets/portfolio/ticket.jpg';
 import detection from '../assets/portfolio/imaged.jpeg';
+import calculator from '../assets/portfolio/calc.jpeg';
+import udemy from '../assets/portfolio/udemy.png';
+import nptel from '../assets/portfolio/NPTEL.jpeg';
+import coursera from '../assets/portfolio/coursera.png';
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      text: 'Bitcoin Price Prediction', 
+      text: 'Bitcoin Price Prediction - Flask, Deep Learning', 
       src: price,
       href: 'https://drive.google.com/drive/folders/1EX-K_1uZupIcVvV94jIfwf5qIkPAn-43?usp=sharing',
     },
     {
       id: 2,
-      text: 'Spam Message Detection', 
+      text: 'Spam Message Detection - Machine Learning, Naive-bayers', 
       src: spam,
       href: 'https://github.com/Jimil1407/spam_detection.git',
     },
     {
       id: 3,
-      text: 'Ticket Enquiry System', 
+      text: 'Ticket Enquiry System - React, MongoDB, Node.js, Render', 
       src: ticket,
       href: 'https://github.com/Jimil1407/mern_proj.git',
     },
     {
       id: 4,
-      text: 'Image Detection Model', 
+      text: 'Image Detection Model - Tensorflow, Deep Learning', 
       src: detection,
       href: 'https://github.com/Jimil1407/image_detection.git',
+    },
+    {
+      id: 5,
+      text: 'Calculator - React, HTML/CSS, JS', 
+      src: calculator,
+      href: 'https://github.com/Jimil1407/calculator',
+    },
+    {
+      id: 6,
+      text: 'Certficate - Web Development', 
+      src: udemy,
+      href: 'https://www.linkedin.com/feed/update/urn:li:activity:7056981273428324352/?originTrackingId=%2Fz3YVAwwR4mNtYcWPAl0Tw%3D%3D',
+    },
+    {
+      id: 7,
+      text: 'Certficate - Python from NPTEL-IIT Madras', 
+      src: nptel,
+      href: 'https://drive.google.com/file/d/1AaMLIe0hcdwRiGJcNctJkGMz9Vuegyum/view?usp=sharing',
+    },
+    {
+      id: 8,
+      text: 'Certficate - Data Foundations from Google', 
+      src: coursera,
+      href: 'https://www.linkedin.com/feed/update/urn:li:activity:7015252666234662912/?originTrackingId=hXc%2FDS0qQ6eSxXmKrbx9Og%3D%3D',
     },
   ];
 
@@ -40,10 +68,10 @@ const Portfolio = () => {
           <p className="py-6">Check out my projects and Certificates</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+        <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-8 px-12 sm:px-0">
           {portfolios.map(({ id, text, src, href }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-              <p className="text-xl font-bold">{text}</p> {/* Display text field here */}
+              <p className="text-l font-bold">{text}</p> 
               <img src={src} alt="" className="rounded-md duration-200 hover:scale-105" />
               <div className="flex items-center justify-center">
                 <a href={href}>
