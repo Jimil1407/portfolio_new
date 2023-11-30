2b16895fce387c017d022dc57b69ad529970aef9
Jimil Digaswala <92706888+Jimil1407@users.noreply.github.com>
2023-11-30 10:35:38 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -40,18 +40,18 @@ const NavBar = () => {
         <h1 className = "text-5xl font-signature ml-2">Jimil</h1>
     </div>
 
-<ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 z-20">
-  {links.map(({ id, link }) => (
-    <li
-      key={id}
-      className="px-4 cursor-pointer capitalize py-6 text-4xl"
-    >
-      <Link onClick={() => setNav(!nav)} to={link} smooth duration={500}>
-        {link}
-      </Link>
-    </li>
-  ))}
-</ul>
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-white-500 hover:scale-105 duration-200"
+          >
+            <Link to={link} smooth duration={500}>
+              {link}
+            </Link>
+          </li>
+        ))}
+      </ul>
 
 
     <div onClick={() => setNav(!nav)} className = "cursor-pointer pr-4 z-10 text-gray-500 md:hidden">
