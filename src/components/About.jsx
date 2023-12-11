11474a9a90330603903234b4088a7e268d3dc6d4
Jimil Digaswala <92706888+Jimil1407@users.noreply.github.com>
2023-12-11 17:25:49 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,19 +14,24 @@ const About = () => {
         </div>
 
         <p className="text-xl mt-20">
-        I'm Jimil Digaswala, a cse undergrad student. Currently, 
-                I am pursuing Computer Science with AI and Machine Learning.
-                I love to create and design the websites, I'm a learning front-end developer. 
-                I use HTML,CSS,Javascript,Tailwind and React to create my webites. 
-                I am also well versed with Data Structures and Algorithms.
+        I am Jimil Digaswala, a dedicated Computer Science and Engineering undergraduate student with a specialization in Artificial Intelligence and Machine Learning. 
+          My academic pursuits align with my passion for technology, particularly in the realm of web development.
+          In addition to my web development skills, I have a solid foundation in Data Structures and Algorithms. This knowledge enhances my problem-solving capabilities, 
+          ensuring that the solutions I design are not only visually engaging but also robust and efficient.
         </p>
 
         <br />
 
         <p className="text-xl">
-        Despite being a nerd, I love watching sports especially cricket and football. 
-                That is like one of my favourite things to do in between breaks.
-                I'm a huge Cristiano and MS Dhoni fan. I personally also love to play sports and keep myself fit.
+        In addition to my proficiency in web development, I bring hands-on experience in building machine-learning models. 
+          Having successfully developed and implemented machine learning solutions, I am adept at leveraging data to derive valuable 
+          insights and enhance the functionality of digital applications.
+
+          <br />
+
+        Currently, I am honing my skills in web development, continually exploring emerging technologies, and staying abreast of industry trends.
+          My experience in machine learning serves as a valuable asset, allowing me to approach projects with a holistic
+          perspective and an understanding of both front-end design and intelligent data-driven solutions.
 
         </p>
       </div>
